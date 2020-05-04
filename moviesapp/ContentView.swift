578b1bc5e5de6578d9fcfb18c7f89057cf810d9b
nada <64605593+nadaalmatouq@users.noreply.github.com>
2020-05-05 00:20:40 +0300

    ب

--- moviesapp/ContentView.swift
+++ moviesapp/ContentView.swift
@@ -9,8 +9,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+        List{
+            
+            NavigationLink(destination: Image(uiImage: #imageLiteral(resourceName: "a bugs life"))){
+            HStack{
+              
+                Image(uiImage:   #imageLiteral(resourceName: "a bugs life")).resizable().frame(width: 90, height: 90).clipShape(Capsule())
+                
+                
+                
+                VStack{
+                    
+                    Text("a Bug's Life").font(.largeTitle)
+                    Text("Flick,Hopper")
+                    
+                    
+                }
+                
+                
+                
+                
+                
+            }//HStack1
+                
+            }//Navigationlink1
+            HStack{
+              
+                Image(uiImage:   #imageLiteral(resourceName: "birds of prey")).resizable().frame(width: 90, height: 90).clipShape(Capsule())
+                
+                
+                
+                VStack{
+                    
+                    Text("Birds of Prey").font(.largeTitle)
+                    Text("Harley Quin,Cassandra")
+                    
+                    
+                }
+                
+                
+                
+                
+                
+            }//HStack2
+            HStack{
+              
+                Image(uiImage:   #imageLiteral(resourceName: "suicidesquad.jpeg")).resizable().frame(width: 90, height: 90).clipShape(Capsule())
+                
+                
+                
+                VStack{
+                    
+                    Text("Suicide Squad").font(.largeTitle)
+                    Text("Joker,Harley Quin")
+                    
+                    
+                }
+                
+                
+                
+                
+                
+            }//HStack1
+            
+            
+            
+            
+            
+            
+            }
+        }
     }
 }
 
---
+++ moviesapp/Preview Content/bugsLife.swift
@@ -0,0 +1,24 @@
+//
+//  bugsLife.swift
+//  moviesapp
+//
+//  Created by Nada Alm on 5/4/20.
+//  Copyright © 2020 Nada Alm. All rights reserved.
+//
+
+import SwiftUI
+
+struct bugsLife: View {
+    var body: some View {
+        
+        
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct bugsLife_Previews: PreviewProvider {
+    static var previews: some View {
+        bugsLife()
+    }
+}
